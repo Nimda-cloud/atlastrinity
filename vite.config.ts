d4992872e337e84009fb72cd6b5f92ab5a9e6982
Nimda-cloud <nimda-cloud@users.noreply.github.com>
2026-02-01 12:42:44 -0300

    fix(maps): allow unsafe-eval in CSP and fix Vite env loading

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [react()],
     base: './',
     root: 'src/renderer',
+    envDir: '../../',
     build: {
         outDir: '../../dist/renderer',
         emptyOutDir: true,
